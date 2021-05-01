302b323254145f50d6050dcd9a1d729a4d89861e
Android-Penguin <mail@shauncb.xyz>
2021-05-01 20:11:22 +1200

    Code from Google Cloud
    - Code that was hosted on Google Cloud under the free trial period.
    
    - Currently broken as images are stuck in the GCloud bucket until I fix account.

---
+++ image_list2.js
@@ -0,0 +1,25 @@
+var image_list2 = [
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+      "filepath":"content_images/test_penguin.png",
+      "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+      "filepath":"content_images/test_penguin.png",
+      "onclick":"modalOpen('content_images/test_penguin.png')"},
+
+  {"item_number":1, "name":"Penugin", "show":"none",
+    "filepath":"content_images/test_penguin.png",
+    "onclick":"modalOpen('content_images/test_penguin.png')"},
+];
